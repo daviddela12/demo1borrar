07e5039cdee51c8920560e3cc9ffeca2e9d47194
daviddela <daviddelacruzvirgil@gmail.com>
2021-11-25 22:12:14 +0100

    Order and Product models added

--- src/main/java/com/example/demo1borrar/domain/Product.java
+++ src/main/java/com/example/demo1borrar/domain/Product.java
@@ -1,4 +1,23 @@
 package com.example.demo1borrar.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idProduct;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
 }
