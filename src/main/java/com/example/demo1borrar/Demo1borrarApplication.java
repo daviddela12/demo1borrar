c37a100a02a757b0f8e25b6ce0b133f0e6adc04c
daviddela <daviddelacruzvirgil@gmail.com>
2021-11-25 21:55:43 +0100

    init commit

---
+++ src/main/java/com/example/demo1borrar/Demo1borrarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo1borrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1borrarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1borrarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo1borrar/Demo1borrarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo1borrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1borrarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
